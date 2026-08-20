8b195b57834658b86f774a6294d6255330ba884a
zRayhhh <150665647+zRayhhh@users.noreply.github.com>
2026-08-20 15:35:08 +0800

    Completed rename, greet, exit; updated .gitignore for .class files

---
+++ src/main/java/Ayre.java
@@ -0,0 +1,18 @@
+public class Ayre {
+    public static void main(String[] args) {
+        String banner = """
+                <<Main System: Activating Support Mode.>>
+                     █████╗ ██╗   ██╗██████╗ ███████╗
+                    ██╔══██╗╚██╗ ██╔╝██╔══██╗██╔════╝
+                    ███████║ ╚████╔╝ ██████╔╝█████╗
+                    ██╔══██║  ╚██╔╝  ██╔══██╗██╔══╝
+                    ██║  ██║   ██║   ██║  ██║███████╗
+                    ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝
+                    .~"~.__.~"~.__.~"~.__.~"~.__.~"~.\n""";
+        String greeting = "~ Hello, Raven. What shall we do today?\n";
+        String goodbye = "~ Terminating connection. See you again, Raven.";
+
+        System.out.println(banner + greeting);
+        System.out.println(goodbye);
+    }
+}
