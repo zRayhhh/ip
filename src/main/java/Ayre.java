b4dba170c619bd50d53d1059bf334b180e0758ab
zRayhhh <150665647+zRayhhh@users.noreply.github.com>
2026-08-20 23:13:37 +0800

    Add minimal error handling; input parsing separated into own method

--- src/main/java/Ayre.java
+++ src/main/java/Ayre.java
@@ -1,11 +1,39 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Ayre {
     private TaskList tasks;
 
     public Ayre() {
         this.tasks = new TaskList();
+    }
+
+    public String parseInput(String input) throws InvalidCommandException, InsufficientArgumentsException {
+        String[] body = input.trim().split("\s", 2);
+        String cmd = body[0].toLowerCase();
+        if (cmd.equals("bye")) {
+            System.out.println("~ Terminating connection. See you again, Raven.");
+        } else if (cmd.equals("list")) {
+            System.out.print(tasks);
+        } else if (cmd.equals("unmark")) {
+            tasks.unmarkTask(Integer.parseInt(body[1]) - 1);
+        } else if (cmd.equals("mark")) {
+            tasks.markTask(Integer.parseInt(body[1]) - 1);
+        } else {
+            if (cmd.equals("todo")) {
+                if (body.length < 2) throw new InsufficientArgumentsException("Mission body not found.");
+                tasks.addTask(new Todo(body[1]));
+            } else if (cmd.equals("deadline")) {
+                String[] params = body[1].split(" /by ");
+                tasks.addTask(new Deadline(params[0], params[1]));
+            } else if (cmd.equals("event")) {
+                String[] params = body[1].split(" /from | /to ");
+                tasks.addTask(new Event(params[0], params[1], params[2]));
+            } else {
+                throw new InvalidCommandException("Unknown command.");
+            }
+            System.out.print("~ [" + input + "] has been added to the mission list.\n> ");
+        }
+        return cmd;
     }
 
     public void run() {
@@ -22,37 +50,16 @@ public class Ayre {
         String goodbye = "~ Terminating connection. See you again, Raven.";
         System.out.print(banner + greeting);
 
-        String userInput;
-        String cleanInput;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            userInput = scanner.nextLine().trim();
-            cleanInput = userInput.toLowerCase();
-            if (cleanInput.equals("bye")) {
-                System.out.println(goodbye);
-                break;
-            } else if (cleanInput.equals("list")) {
-                System.out.print(tasks);
-            } else if (cleanInput.contains("unmark")) {
-                tasks.unmarkTask(Integer.parseInt(cleanInput.split("\s")[1]) - 1);
-
-            } else if (cleanInput.contains("mark")) {
-                tasks.markTask(Integer.parseInt(cleanInput.split("\s")[1]) - 1);
-            } else {
-                String[] input = userInput.split("\s", 2);
-                input[0] = input[0].toLowerCase();
-                if (input[0].equals("todo")) {
-                    tasks.addTask(new Todo(input[1]));
-                } else if (input[0].equals("deadline")) {
-                    String[] params = input[1].split(" /by ");
-                    tasks.addTask(new Deadline(params[0], params[1]));
-                } else if (input[0].equals("event")) {
-                    String[] params = input[1].split(" /from ");
-                    String[] times = params[1].split(" /to ");
-                    tasks.addTask(new Event(params[0], times[0], times[1]));
-                }
-                System.out.print("~ [" + userInput + "] has been added to the mission list.\n> ");
+            try {
+                String cmd = parseInput(scanner.nextLine());
+                if (cmd.equals("bye")) break;
+            } catch (InsufficientArgumentsException e) {
+                System.out.print("~ Raven, I'm not seeing the mission details. Please try again.\n> ");
+            } catch (InvalidCommandException e) {
+                System.out.print("~ ...Raven, this command was not found in the Coral Collective. Was it a mistake?\n> ");
             }
         }
     }
