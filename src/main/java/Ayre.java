1c8674edf078253bc5664aa5176927d0b70af418
zRayhhh <150665647+zRayhhh@users.noreply.github.com>
2026-08-20 16:42:06 +0800

    Added user task list: task add and list print

--- src/main/java/Ayre.java
+++ src/main/java/Ayre.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Ayre {
     public static void main(String[] args) {
@@ -15,15 +16,22 @@ public class Ayre {
         String greeting = "~ Hello, Raven. What shall we do today?\n> ";
         String goodbye = "~ Terminating connection. See you again, Raven.";
         System.out.print(banner + greeting);
-        
+
+        ArrayList<String> lst = new ArrayList<>();
         String userInput;
         while (true) {
             userInput = scanner.nextLine();
             if (userInput.compareToIgnoreCase("bye") == 0) {
                 System.out.println(goodbye);
                 break;
+            } else if (userInput.compareToIgnoreCase("list") == 0) {
+                for (int i = 1; i <= lst.size(); i++) {
+                    System.out.println(i + ". " + lst.get(i - 1));
+                }
+                System.out.print("> ");
             } else {
-                System.out.print("~ " + userInput + "\n> ");
+                lst.add(userInput);
+                System.out.print("~ [" + userInput + "] has been added to the mission list.\n> ");
             }
         }
     }
