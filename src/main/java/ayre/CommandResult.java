48b6b850d9b706c5f268ae18543857a067d61d19
zRayhhh <150665647+zRayhhh@users.noreply.github.com>
2026-08-29 19:32:31 +0800

    Remove legacy commented block in CommandResult

--- src/main/java/ayre/CommandResult.java
+++ src/main/java/ayre/CommandResult.java
@@ -2,21 +2,4 @@ package ayre;
 
 import ayre.enums.AyreStatus;
 
-//public final class CommandResult {
-//    private final String MESSAGE;
-//    private final AyreStatus STATUS;
-//
-//    public CommandResult(String msg, AyreStatus status) {
-//        this.MESSAGE = msg;
-//        this.STATUS = status;
-//    }
-//
-//    public String getMessage() {
-//        return this.MESSAGE;
-//    }
-//
-//    public AyreStatus getStatus() {
-//        return this.STATUS;
-//    }
-//}
 public record CommandResult(String message, AyreStatus status) {}
